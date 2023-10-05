087a0f555a388e232ed05282837b94e7d622ec62
ShonKhundiashvili <shonkhundiashvili@gmail.com>
2023-10-05 23:11:21 +0300

    adding comments about the code

--- createSocialMedia.php
+++ createSocialMedia.php
@@ -1,13 +1,17 @@
 <?php
 include('./connection.php');
 
+//Calling the database instance
 $db = new Database("127.0.0.1", "root", "", "data");
+
+//Getting the connection of sqli for communication with the database
 $mysqli = $db->getConnection();
 
 $imageUrl = 'https://cdn2.vectorstock.com/i/1000x1000/23/81/default-avatar-profile-icon-vector-18942381.jpg';
 $directoryPath = '/Applications/XAMPP/xamppfiles/htdocs/inManage';
 $savePath = $directoryPath . '/savedImg.jpg';
 
+//Saving the picture in server (my computer)
 if (!file_exists($savePath)) {
     $imageContent = file_get_contents($imageUrl);
 
@@ -24,15 +28,19 @@ if (!file_exists($savePath)) {
     }
 }
 
+//Checking connection to mysql before executing the query
 if ($mysqli->connect_error) {
     die("Database connection failed: " . $mysqli->connect_error);
 }
 
+//Question number 5 query to return active users and their posts
 $query = "SELECT u.name as name, p.title as title, p.body as body
 FROM users u JOIN posts p ON p.user_id = u.id
 WHERE u.active = 1";
 
+//Execute
 $result = $mysqli->query($query);
+
 
 if ($result) {
     while ($row = $result->fetch_assoc()) {
